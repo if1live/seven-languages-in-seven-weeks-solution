f20e79b4f59fda844547ca900238121e3b40282a
if1live <libsora25@gmail.com>
2015-06-24 18:23:26 +0900

    무한루프 수정. hanoi 참고용 코드

--- prolog/day_2/factorial.pl
+++ prolog/day_2/factorial.pl
@@ -1,5 +1,11 @@
 factorial(1, 1).
+
 factorial(Factorial, Num) :-
+	Num < 1,
+	Factorial = 0.
+
+factorial(Factorial, Num) :-
+	Num > 1,
 	PrevIndex is Num - 1,
 	factorial(PrevFactorial, PrevIndex),
 	Factorial is Num * PrevFactorial.
--- prolog/day_2/fib.pl
+++ prolog/day_2/fib.pl
@@ -2,7 +2,12 @@ fibonacci(0, 0).
 fibonacci(1, 1).
 fibonacci(1, 2).
 
+/*
+Index의 범위를 제한하지 않으면 모든 값을 얻으려고할때
+무한반복이 발생한다
+*/
 fibonacci(Fibonacci, Index) :-
+	Index >= 2,
 	IndexPrev1 is Index - 1,
 	IndexPrev2 is Index - 2,
 	fibonacci(Prev1, IndexPrev1),
